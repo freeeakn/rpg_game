a74bba9eba13d133f0a863f4d3051402d66b0d7f
Machine Enger <freakn@Arthurs-MacBook-Air.local>
2023-06-04 20:38:04 +0700

    add circle

--- src/main.c
+++ src/main.c
@@ -1,17 +1,41 @@
-// machinee [2023], Kalsky
+// machinee, Kalsky [2023]
 // develop branch
 #include "raylib.h"
-#include <stdio.h>
+
+//! constants
+#define screenWidth 800
+#define screenHeight 450
+
 
 int main() {
 
-  InitWindow(800, 450, "raylib [core] example - basic window");
+  //! variables and start configuration
+
+  Vector2 ballPosition = {
+    (float)screenWidth / 2,
+    (float)screenHeight / 2,
+  };
+
+  // set startup fps
+  SetTargetFPS(30);
+
+  InitWindow(screenWidth, screenHeight, "Game");
 
   while (!WindowShouldClose()) {
+
+    // Update
+    //----------------------------------------------------------------------------------
+    if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
+    if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
+    if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
+    if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+    //----------------------------------------------------------------------------------
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
+    DrawText("", 190, 200, 20,
              LIGHTGRAY);
+    DrawCircleV(ballPosition, 50, MAROON);
     EndDrawing();
   }
 
